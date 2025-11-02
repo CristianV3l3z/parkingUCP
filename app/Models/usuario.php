4a5e40cfbce191b632939f724f2a719c908202f0
Miguel Angel <136278328+AngelmarkX@users.noreply.github.com>
2025-11-02 16:27:00 -0500

    Update usuario en mayus

--- app/Models/usuario.php
+++ app/Models/usuario.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 
-class usuario extends Authenticatable
+class Usuario extends Authenticatable
 {
     use HasFactory, Notifiable;
 
