8022f41898cdfa1083f7a25ac17482ac83a1f5d7
Miguel Angel <136278328+AngelmarkX@users.noreply.github.com>
2025-11-02 16:20:35 -0500

    Cambio en modelo Usuario minuscula

--- app/Models/usuario.php
+++ app/Models/usuario.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 
-class Usuario extends Authenticatable
+class usuario extends Authenticatable
 {
     use HasFactory, Notifiable;
 
