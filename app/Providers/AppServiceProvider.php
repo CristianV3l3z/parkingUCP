634c56a13c49597617e79d1879f67644e38d3825
Miguel Angel <136278328+AngelmarkX@users.noreply.github.com>
2025-11-02 00:30:05 -0500

    Update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 👈 AGREGA ESTA LÍNEA
 
 class AppServiceProvider extends ServiceProvider
 {
