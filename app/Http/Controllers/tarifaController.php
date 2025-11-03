5650cb044486a1412dababb6c08d565d2f0368d8
Miguel Angel <136278328+AngelmarkX@users.noreply.github.com>
2025-11-02 19:06:17 -0500

    T mayuscula TarifaController

--- app/Http/Controllers/tarifaController.php
+++ app/Http/Controllers/tarifaController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Tarifa;
+use App\Models\tarifa;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
